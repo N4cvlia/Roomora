0d0f4a91c3943dc68880f234b5babe26ec178059
Nacvlia <kaxa487@gmail.com>
2025-09-28 00:05:44 +0400

    Made burger menu

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
@@ -10,9 +10,26 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class NavbarComponent {
   public isScrolled = false;
+  public burgerVisib: boolean = false;
+
+  constructor(private routing: Router) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 79.5;
+  }
+  @ViewChild("BurgerDropdown") profileDropdown! : ElementRef;
+  @HostListener("document:click", ['$event'])
+  onDocumentClick(event : Event) {
+    if(!this.profileDropdown.nativeElement.contains(event.target)) {
+      this.burgerVisib = false
+    }
+  }
+  BurgerDrop(event: Event) {
+    event.stopPropagation();
+    this.burgerVisib = !this.burgerVisib
+  }
+  BurgerClose() {
+    this.burgerVisib = false
   }
 }
